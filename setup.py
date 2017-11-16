84b0eea7fe9665c464a293aff459f179566a3cdb
Kevin (Pololu) <dev-kevin@pololu.com>
2017-11-16 12:43:08 -0800

    fix setup.py description and add license

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='pololu_drv8835_rpi',
       version='2.0.0',
-      description=('Library for the Pololu DRV8835 Dual Motor'
+      description=('Library for the Pololu DRV8835 Dual Motor '
                    'Driver Kit for Raspberry Pi Model B+'),
       url='http://www.pololu.com/product/2753',
       py_modules=['pololu_drv8835_rpi'],
